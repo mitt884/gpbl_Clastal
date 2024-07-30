4ecae80b6cc2a64855d1669ccf7fe2da7b6fc8d9
KuanNiisan <haibakhongtam2004@gmail.com>
2024-07-30 21:03:24 +0700

    add build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,6 @@
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r backend/requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 backend/manage.py collectstatic --no-input
+python3 backend/manage.py migrate
